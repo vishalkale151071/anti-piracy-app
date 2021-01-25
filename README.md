ccc268a4f6aba1533a1c293d1f160ed89bd87800
Vishal <vishalkale151071@gmail.com>
2021-01-25 21:33:50 +0530

    first Push.

---
+++ README.md
@@ -0,0 +1,35 @@
+# electron-deep-linking-macOS-win32
+[![codebeat badge](https://codebeat.co/badges/62d56692-2100-4dee-ad02-80fdb2fd3838)](https://codebeat.co/projects/github-com-oikonomopo-electron-deep-linking-mac-win-master)
+
+
+Open app and pass parameters with deep linking using Electron (macOS/win32).
+
+This app is registered as ```myapp://```. For example, open Safari browser, enter ```myapp://param``` at address bar. Then 'electron-deep-linking-mac-win' app will start and provided url will be printed to dev console!
+
+
+![](https://github.com/oikonomopo/electron-deep-linking-osx/blob/master/electron-deeplinking-osx-example.gif)
+
+
+Project is based to [Quick Start Guide](http://electron.atom.io/docs/tutorial/quick-start) within the Electron documentation, then followed [‘electron-builder’ quick-setup-guide](https://github.com/electron-userland/electron-builder#quick-setup-guide) to produce macOS/win32 installers.
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/oikonomopo/electron-deep-linking-mac-win.git
+# Go into the repository
+cd electron-deep-linking-mac-win
+# Install dependencies
+npm install
+# Run the app
+npm start
+# Produce installer
+npm run dist
+```
+After running the installer (at electron-deep-linking-mac-win/dist/), you can try to open 'electron-deep-linking-mac-win' app with deep linking, by entering ```myapp://param``` at Safari address-bar. At chrome it will work by embedding the custom url in a link: f.e. ```<a href="myapp://param">Open app</a>```
+
+## License
+
+[GNU GPLv3](LICENSE.md)
---
+++ main.js
@@ -0,0 +1,118 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Deep linked url used to launch app
+let deeplinkingUrl = [];
+
+// Force Single Instance Application
+const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+
+  // Protocol handler for win32
+  // argv: An array of the second instance’s (command line / deep linked) arguments
+  if (process.platform == 'win32') {
+    // Keep only command line / deep linked arguments
+    deeplinkingUrl = argv.slice(1)[1];
+  }
+  logEverywhere("app.makeSingleInstance# " + deeplinkingUrl);
+
+  if (mainWindow) {
+    if (mainWindow.isMinimized()){
+      mainWindow.restore();
+      mainWindow.focus();
+    }
+  }
+});
+
+if (shouldQuit) {
+    app.quit();
+    return;
+}
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Protocol handler for win32
+  if (process.platform == 'win32') {
+    // Keep only command line / deep linked arguments
+    deeplinkingUrl = process.argv.slice(1)[1];
+  }
+
+  if(!deeplinkingUrl){
+    deeplinkingUrl = 'activated';
+  }
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  })
+
+  const components = url.parse(deeplinkingUrl, true);
+  logEverywhere("Scheme : " + components.protocol);
+  logEverywhere("Host : " + components.host);
+  logEverywhere("Pathname : " + components.pathname);
+  const params = components.query;
+  logEverywhere("Query : " + params.t);
+
+  if(components.protocol == "celestial:" && components.host == "localhost:5000"){
+    logEverywhere("Ok.");
+  }else{
+    app.exit();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+if(process.platform == "win32"){
+  app.setAsDefaultProtocolClient('celestial', process.execPath, [path.resolve(process.argv[1])]);
+}else{
+  app.setAsDefaultProtocolClient('celestial');
+}
+
+
+// Protocol handler for osx
+app.on('open-url', function (event, url) {
+  event.preventDefault();
+  deeplinkingUrl = url;
+  logEverywhere("open-url# " + deeplinkingUrl);
+});
+
+// Log both at dev console and at running node console instance
+function logEverywhere(s) {
+    console.log(s);
+    if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.executeJavaScript(`console.log("${s}")`);
+    }
+}
