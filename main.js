d4adb60abceaf4612fed010d59f7a0f6082e684e
vishal kale <40729195+vishalkale151071@users.noreply.github.com>
2021-01-27 16:15:58 +0530

    All functionality Done.

--- main.js
+++ main.js
@@ -1,38 +1,53 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const { Http2ServerRequest } = require('http2');
 const path = require('path');
 const url = require('url');
-const logEverywhere = require('./utilities');
+const  { logEverywhere, decomposeUrl, intiPlugin, sendReport } = require('./utilities');
+const Store = require('electron-store');
 
+const store = new Store();
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
 // Deep linked url used to launch app
-let deeplinkingUrl = [];
+let deeplinkingUrl = null;
 
 // Force Single Instance Application
-const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
-  // Someone tried to run a second instance, we should focus our window.
+const gotTheLock = app.requestSingleInstanceLock()
 
-  // Protocol handler for win32
-  // argv: An array of the second instance’s (command line / deep linked) arguments
-  if (process.platform == 'win32') {
-    // Keep only command line / deep linked arguments
-    deeplinkingUrl = argv.slice(1)[1];
-  }
-  logEverywhere("app.makeSingleInstance# " + deeplinkingUrl, mainWindow);
-
-  if (mainWindow) {
-    if (mainWindow.isMinimized()){
-      mainWindow.restore();
-      mainWindow.focus();
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, args) => {
+    // Someone tried to run a second instance, we should focus our window.
+    const  { pathName, hostName, params } = decomposeUrl(args.slice()[2]);
+    
+    if(pathName == "/quit"){
+      app.quit();
     }
-  }
-});
 
-if (shouldQuit) {
-    app.quit();
-    return;
+    switch(pathName){
+      case '/quit':
+        app.quit;
+        break;
+      
+      case '/start-detecting':
+        let token = store.get('token'); 
+        if(!token){
+          app.quit();
+        }
+        sendReport(mainWindow, hostName, token);
+        break;
+    }
+
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+
+  // Create myWindow, load the rest of the app, etc...
+  app.on('ready', createWindow);
 }
 
 function createWindow () {
@@ -62,44 +77,43 @@ function createWindow () {
   }
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
+    store.clear();
     mainWindow = null;
   })
 
-  const components = url.parse(deeplinkingUrl, true);
-  logEverywhere("Scheme : " + components.protocol, mainWindow);
-  logEverywhere("Host : " + components.host, mainWindow);
-  logEverywhere("Pathname : " + components.pathname, mainWindow);
-  const params = components.query;
-  logEverywhere("Query : " + params.t, mainWindow);
+  const  { protocolName, pathName, hostName, params} = decomposeUrl(deeplinkingUrl);
 
-  if(components.protocol == "celestial:" && components.host == "localhost:5000"){
+  if(protocolName == "celestial:" && hostName == "localhost:5000" && pathName ==  '/start' && params.t){
     logEverywhere("Ok.", mainWindow);
+    intiPlugin(hostName, params.t, mainWindow);
+    store.set('token', params.t)
   }else{
-    app.exit();
+    store.clear();
+    app.quit();
   }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+//app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-});
+// app.on('window-all-closed', function () {
+//   // On OS X it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+// app.on('activate', function () {
+//   // On OS X it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (mainWindow === null) {
+//     createWindow();
+//   }
+// });
 
 if(process.platform == "win32"){
   app.setAsDefaultProtocolClient('celestial', process.execPath, [path.resolve(process.argv[1])]);
@@ -107,10 +121,14 @@ if(process.platform == "win32"){
   app.setAsDefaultProtocolClient('celestial');
 }
 
-
 // Protocol handler for osx
-app.on('open-url', function (event, url) {
-  event.preventDefault();
-  deeplinkingUrl = url;
-  logEverywhere("open-url# " + deeplinkingUrl, mainWindow);
+// app.on('open-url', function (event, url) {
+//   event.preventDefault();
+//   deeplinkingUrl = url;
+//   logEverywhere("open-url# " + deeplinkingUrl, mainWindow);
+// });
+
+ipcMain.on('close', (event, arg) => {
+  store.clear();
+  app.quit();
 });
--- utilities.js
+++ utilities.js
@@ -1,3 +1,7 @@
+const url = require('url');
+const axios = require('axios').default;
+const psList = require('ps-list');
+
 function logEverywhere(s, mainWindow) {
     console.log(s);
     if (mainWindow && mainWindow.webContents) {
@@ -5,4 +9,73 @@ function logEverywhere(s, mainWindow) {
     }
 }
 
-module.exports = logEverywhere
+function decomposeUrl(_url){
+    const components = url.parse(_url, true);
+    const params = components.query;
+    const protocolName = components.protocol;
+    const hostName = components.host;
+    const pathName = components.pathname;
+
+    return {
+        protocolName,
+        hostName,
+        pathName,
+        params
+    }
+}
+
+async function intiPlugin(_hostName, _token, mainwindow){
+    let res = await axios.request({
+        url: `http://${_hostName}/plugin/init`,
+        method: 'post',
+        headers: {
+            Cookie: `connect.sid=s%3A${_token}`
+        }
+    });
+
+    logEverywhere(res.status, mainwindow);
+}
+
+async function sendReport(window, _hostName, _token){
+    let status;
+    let tool;
+    const blackList = ['obs64.exe'];
+    let flag = false;
+    await psList().then(data => {
+        for(let i=0;i<data.length;i++){
+            let item = data[i];
+            if(blackList.includes(item.name)){
+                flag = true;
+                logEverywhere("Found : " + item.name, window);
+                status = "Alert";
+                tool = item.name;
+                break;
+            }
+        }
+    });
+    if(!flag){
+        status = "Ok";
+        tool = ''
+    }
+    logEverywhere("Data : " + status + ' ' + tool, window);
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Cookie: `connect.sid=s%3A${_token}`
+        }
+    }
+    let res = await axios.post(
+        `http://${_hostName}/plugin/log`,
+        { status, tool },
+        config
+    )   
+
+    logEverywhere(res.status, window);
+}
+
+module.exports = {
+    logEverywhere: logEverywhere,
+    decomposeUrl: decomposeUrl,
+    intiPlugin: intiPlugin,
+    sendReport: sendReport
+}   
